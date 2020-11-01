db499969675685587ed39920723fd963803a37ff
4bdeladim <abdeladim.frontend@gmail.com>
2020-11-01 13:10:25 +0100

    final code

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+          <Navbar />
+          <Route path='/Website' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+      </Router>
+    </>
+  );
+}
+
+export default App;
